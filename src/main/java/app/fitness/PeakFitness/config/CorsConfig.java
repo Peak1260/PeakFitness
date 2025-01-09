5359c981561f18e2cc209dc78720e391ab73d4dc
Peak <peak1260@gmail.com>
2025-01-08 16:35:53 -0800

    adding allowed origins to CORS

--- src/main/java/app/fitness/PeakFitness/config/CorsConfig.java
+++ src/main/java/app/fitness/PeakFitness/config/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://peakfitness.up.railway.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
